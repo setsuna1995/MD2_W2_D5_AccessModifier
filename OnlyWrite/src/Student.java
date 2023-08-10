08ad0bbc39f18d788d4cdd408b403843f00929cf
setsuna1995 <kien05071995@gmail.com>
2023-08-10 22:12:19 +0700

    only write

---
+++ OnlyWrite/src/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+   static String name = "Harry";
+   static String classes = "Gryffindor";
+    Student () {}
+
+    static String setName() {
+        return name;
+    }
+
+    static String setClasses() {
+        return classes;
+    }
+}
